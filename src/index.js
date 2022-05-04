d1c64c782cd61ac0a4becf6ac7e73a5b8c06385e
Kosteksal <kosteks@outlook.com>
2022-05-04 19:27:08 +0300

    feat: add keyboard

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ const body = document.querySelector('body');
 
 document.addEventListener('DOMContentLoaded', builder);
 
+const buttons = [
+  ['`', '1','2','3','4','5','6','7','8','9','0','-','=','BackSpace',],
+  ['Tab','q','w','e','r','t','y','u','i','o','p','[',']','&#92;',],
+  ['Caps','a','s','d','f','g','h','j','k','l',';','"','Enter',],
+  ['Shift','z','x','c','v','b','n','m',',','.','/','&#8593;','Shift',],
+  ['Ctrl','&#8862;','Alt','Space','Alt','Ctrl','&#8592;','&#8595;','&#8594;',]
+]
+
 function builder () {
   const container = document.createElement('div');
   container.classList.add('container');
@@ -25,4 +33,37 @@ function builder () {
   container.append(textOS);
   container.append(textLang);
 
+  
+  
+
+  buttons.forEach((i) => {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    keyboardContainer.append(row);
+    i.forEach((e) => {
+      const button = document.createElement('div');
+      button.classList.add('button');
+      button.innerHTML = e;
+      row.append(button);
+    });
+  })
+  longer();
+}
+
+function longer () {
+  const btns = document.querySelectorAll('.button');
+  btns[13].classList.add('backspace');
+  btns[14].classList.add('tab');
+  btns[28].classList.add('caps');
+  btns[40].classList.add('enter');
+  btns[41].classList.add('shift');
+  btns[53].classList.add('shift');
+  btns[57].classList.add('space');
+  console.log(btns)
+}
+
+document.addEventListener('keydown', keyCode());
+
+function keyCode (event) {
+    console.log(event.key)
 }
