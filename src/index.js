d1543aff2b757a9f2326c5d641b985b89c6aea4d
Kosteksal <kosteks@outlook.com>
2022-05-09 12:41:08 +0300

    fix: lint mistakes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const body = document.querySelector('body');
 
-document.addEventListener('DOMContentLoaded', builder);
-
 let buttons = [
   ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BackSpace'],
   ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '&#92;'],
@@ -11,59 +9,6 @@ let buttons = [
 ];
 
 const langMem = 'en';
-
-function builder() {
-  const container = document.createElement('div');
-  container.classList.add('container');
-  const title = document.createElement('h1');
-  title.classList.add('title');
-  title.innerText = 'Virtual Keyboard';
-  const textArea = document.createElement('textarea');
-  textArea.classList.add('textarea');
-  textArea.innerHTML = '';
-  textArea.readOnly = 'true';
-  const btnContainer = document.createElement('div');
-  btnContainer.classList.add('btnContainer');
-  const clBtn = document.createElement('button');
-  clBtn.classList.add('clBtn');
-  clBtn.innerText = 'Clear';
-  const langBtn = document.createElement('button');
-  langBtn.classList.add('langBtn');
-  langBtn.innerText = 'EN';
-  const keyboardContainer = document.createElement('div');
-  keyboardContainer.classList.add('keyboard-container');
-  const textOS = document.createElement('p');
-  textOS.classList.add('text');
-  textOS.innerText = 'Клавиатура создана в Windows';
-  const textLang = document.createElement('p');
-  textLang.classList.add('text');
-  textLang.innerText = 'Для смены языка: левые Shift и Ctrl';
-  body.append(container);
-  container.append(title);
-  container.append(textArea);
-  container.append(btnContainer);
-  btnContainer.append(clBtn);
-  btnContainer.append(langBtn);
-  container.append(keyboardContainer);
-  container.append(textOS);
-  container.append(textLang);
-
-  buttons.forEach((i) => {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    keyboardContainer.append(row);
-    i.forEach((e) => {
-      const button = document.createElement('button');
-      button.classList.add('button');
-      button.innerHTML = e;
-      row.append(button);
-    });
-  });
-  longer();
-  clicker();
-  clear();
-  langBtn.addEventListener('click', language);
-}
 
 function longer() {
   const btns = document.querySelectorAll('.button');
@@ -79,7 +24,6 @@ function longer() {
 function clicker() {
   const textAr = document.querySelector('.textarea');
   const val = textAr.value;
-  console.log(val);
   const keys = document.querySelectorAll('.button');
   keys.forEach((i) => { i.addEventListener('click', btnClick); });
 }
@@ -213,45 +157,6 @@ function btnClick(event) {
   }
 }
 
-let hold = 'down';
-
-function hardKey(event) {
-  const hardKeys = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
-    'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash',
-    'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
-    'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
-    'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
-
-  const btns = document.querySelectorAll('.button');
-  const key = event.code;
-  const keyR = event.repeat;
-  const keyNum = hardKeys.indexOf(key);
-  const event1 = new Event('click');
-  btns[keyNum].dispatchEvent(event1);
-  btns[keyNum].style.backgroundColor = 'rgba(6, 29, 46, 0.95)';
-  btns[keyNum].style.boxShadow = '1px 2px 33px 2px rgba(6, 29, 46, 0.95)';
-  document.addEventListener('keyup', (even) => {
-    const keyU = even.code;
-    const keyNumU = hardKeys.indexOf(keyU);
-    btns[keyNumU].style.backgroundColor = 'rgb(50 76 100)';
-    btns[keyNumU].style.boxShadow = '1px 2px 3px 2px rgba(34, 60, 80, 0.25)';
-  });
-
-  if (key === 'ShiftLeft' || key === 'ShiftRight') {
-    hold = 'down';
-    if (hold === 'down' && keyR === false) { shifter(); }
-  }
-  document.addEventListener('keyup', (even1) => {
-    const keyi = even1.code;
-    if (keyi === 'ShiftLeft' || keyi === 'ShiftRight') {
-      hold = 'up';
-      if (hold === 'up') { shifter(); }
-    }
-  });
-}
-
-document.addEventListener('keydown', hardKey);
-
 function clear() {
   const clearBtn = document.querySelector('.clBtn');
   const area = document.querySelector('.textarea');
@@ -311,6 +216,100 @@ function language() {
     langSw.innerHTML = 'EN';
   }
 }
+
+function builder() {
+  const container = document.createElement('div');
+  container.classList.add('container');
+  const title = document.createElement('h1');
+  title.classList.add('title');
+  title.innerText = 'Virtual Keyboard';
+  const textArea = document.createElement('textarea');
+  textArea.classList.add('textarea');
+  textArea.innerHTML = '';
+  textArea.readOnly = 'true';
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('btnContainer');
+  const clBtn = document.createElement('div');
+  clBtn.classList.add('clBtn');
+  clBtn.innerText = 'Clear';
+  const langBtn = document.createElement('div');
+  langBtn.classList.add('langBtn');
+  langBtn.innerText = 'EN';
+  const keyboardContainer = document.createElement('div');
+  keyboardContainer.classList.add('keyboard-container');
+  const textOS = document.createElement('p');
+  textOS.classList.add('text');
+  textOS.innerText = 'Клавиатура создана в Windows';
+  const textLang = document.createElement('p');
+  textLang.classList.add('text');
+  textLang.innerText = 'Для смены языка: левые Shift и Ctrl';
+  body.append(container);
+  container.append(title);
+  container.append(textArea);
+  container.append(btnContainer);
+  btnContainer.append(clBtn);
+  btnContainer.append(langBtn);
+  container.append(keyboardContainer);
+  container.append(textOS);
+  container.append(textLang);
+
+  buttons.forEach((i) => {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    keyboardContainer.append(row);
+    i.forEach((e) => {
+      const button = document.createElement('button');
+      button.classList.add('button');
+      button.innerHTML = e;
+      row.append(button);
+    });
+  });
+  longer();
+  clicker();
+  clear();
+  langBtn.addEventListener('click', language);
+}
+
+document.addEventListener('DOMContentLoaded', builder);
+
+let hold = 'down';
+
+function hardKey(event) {
+  const hardKeys = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+    'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash',
+    'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+    'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+    'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+
+  const btns = document.querySelectorAll('.button');
+  const key = event.code;
+  const keyR = event.repeat;
+  const keyNum = hardKeys.indexOf(key);
+  const event1 = new Event('click');
+  btns[keyNum].dispatchEvent(event1);
+  btns[keyNum].style.backgroundColor = 'rgba(6, 29, 46, 0.95)';
+  btns[keyNum].style.boxShadow = '1px 2px 33px 2px rgba(6, 29, 46, 0.95)';
+  document.addEventListener('keyup', (even) => {
+    const keyU = even.code;
+    const keyNumU = hardKeys.indexOf(keyU);
+    btns[keyNumU].style.backgroundColor = 'rgb(50 76 100)';
+    btns[keyNumU].style.boxShadow = '1px 2px 3px 2px rgba(34, 60, 80, 0.25)';
+  });
+
+  if (key === 'ShiftLeft' || key === 'ShiftRight') {
+    hold = 'down';
+    if (hold === 'down' && keyR === false) { shifter(); }
+  }
+  document.addEventListener('keyup', (even1) => {
+    const keyi = even1.code;
+    if (keyi === 'ShiftLeft' || keyi === 'ShiftRight') {
+      hold = 'up';
+      if (hold === 'up') { shifter(); }
+    }
+  });
+}
+
+document.addEventListener('keydown', hardKey);
 
 document.addEventListener('keydown', (ev) => {
   const evCode = ev.code;
